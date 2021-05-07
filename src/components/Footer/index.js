47421b30155b73653985506fbf18b986e5a37d16
Said Mg <saidmghabghab@gmail.com>
2021-05-07 13:05:49 -0400

    added footer / header

---
+++ src/components/Footer/index.js
@@ -0,0 +1,8 @@
+
+export default function Footer() {
+    return (
+        <div style={{ borderTop: '1px solid black', textAlign: 'center' }}>
+
+        <h6 className="mt-2">Shoppies ©2021 Created by <a href="https://portfolio-said.herokuapp.com/" target="_blank" style={{color:'black'}}>Said Mghabghab</a></h6>
+     </div>    );
+  }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,10 @@
 
-export default function ReactLoader() {
+export default function Header() {
   return (
-    <h1>Header</h1>
+         <div className="d-flex bd-highlight" style={{ borderBottom: '0px solid black' }}>
+                <div className="p-3 flex-grow-1 bd-highlight"><h3 style={{ color: 'black', paddingLeft: '5%' }}>The Shoppies</h3></div>
+                <div className="p-3 bd-highlight" style={{ marginRight: '3%' }}><button className="btn form-control" style={{ color: 'black' }}>About</button></div>
+                {/* <div className="p-2 bd-highlight">Third flex item</div> */}
+            </div>
   );
 }
