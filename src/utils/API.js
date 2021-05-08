f11d38f7048f3f303517da977195e09a1bfb8b73
Said Mg <saidmghabghab@gmail.com>
2021-05-07 21:19:02 -0400

    added https

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-anonymous-default-export */
 import axios from "axios";
 
-const BASEURL = "http://www.omdbapi.com/?s=";
+const BASEURL = "https://www.omdbapi.com/?s=";
 const APIKEY = "&apikey=100ee64f";
 const TYPE = "&type=movie"
 
