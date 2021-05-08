c18b5a5b7d15c983dd6a9bc41108be4f1786986a
Said Mg <saidmghabghab@gmail.com>
2021-05-07 21:46:15 -0400

    removed the 'about' button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,7 +3,6 @@ export default function Header() {
   return (
          <div className="d-flex bd-highlight">
                 <div className="p-3 flex-grow-1 bd-highlight text-dark"><h3 className="ps-5">The Shoppies</h3></div>
-                <div className="p-3 bd-highlight me-5"><button className="btn form-control text-dark">About</button></div>
             </div>
   );
 }
